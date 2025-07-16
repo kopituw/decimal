f996baa206b0b53954b9bb9e05b4ad3f78f81feb
Алина <kopituw@gmail.com>
2025-07-16 17:47:36 +0300

    from decimal to float is working

--- src/converters/s21_from_decimal_to_float.c
+++ src/converters/s21_from_decimal_to_float.c
@@ -7,36 +7,39 @@ int s21_from_decimal_to_float(s21_decimal src, float *dst){
 	if (dst == NULL)
 	return CONVERTING_ERROR;
 	
+
 	int sign = get_sign(&src);
-	int scale = get_scale(&src);
+	int scale = get_scale(&src); 
+
+	if (scale > 28) {
+		*dst = 0.0f;
+		return CONVERTING_ERROR;
+	}
 	s21_decimal mantissa = src;
 	mantissa.bit[3] = 0;
 
 	if (mantissa.bit[0] == 0 && mantissa.bit[1] == 0 && mantissa.bit[2] == 0 ) {
 		if (sign){
-		*dst = -0.0f;
+		*dst = -0.0f; printf("-0.0");
 	} else {
-			*dst = 0.0f;
+			*dst = 0.0f; printf("0.0");
 		}
 		return OK;
 	}
-	if (scale > 28){
-		*dst = 0.0f;
-		return CONVERTING_ERROR;
-	}
-
-		while (!(mantissa.bit[2] & 0x80000000) && scale > 0) {
-			shift_left(&mantissa);
-			scale--;
-		}
-		// long double res = 0.0;
-		long double res = (long double)mantissa.bit[0] +
+	long double res;
+	
+	if (mantissa.bit[2] == 0 && mantissa.bit[1] == 0){
+		res = (long double) mantissa.bit[0];
+	} else{
+ 		
+		 res = (long double)mantissa.bit[0] +
 					 (long double)mantissa.bit[1] * 4294967296.0L+
 					 (long double)mantissa.bit[2] * 18446744073709551616.0L;
+	}
 
 		while (scale > 0 ) {
 			res /= 10.0;
-			scale --;
+			 scale --;
 		}
 		
 		if (sign) {
--- src/help/get_scale.c
+++ src/help/get_scale.c
@@ -1,3 +1,5 @@
 #include "../s21_decimal.h"
 
-int get_scale(const s21_decimal *value) { return (char)(value->bit[3] >> 16); }
+int get_scale(const s21_decimal *value) {
+    return (value->bit[3] >> 16) & 0xFF;
+}
