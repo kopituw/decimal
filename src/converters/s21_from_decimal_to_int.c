774f813c2ba2ec3c051cf0875b28fa09cc5dc80b
karleeha <karleeha@student.21-school.ru>
2025-08-24 19:01:20 +0300

    rabotaet 3

--- src/converters/s21_from_decimal_to_int.c
+++ src/converters/s21_from_decimal_to_int.c
@@ -1,27 +1,38 @@
 #include "../s21_decimal.h"
 #include <stdio.h>
 
-int s21_from_decimal_to_int(s21_decimal src, int *dst){
-	if (!dst) { 
-		return CONVERTING_ERROR;
-	}
-	if (get_scale(&src) != 0) { // truncate
-		return CONVERTING_ERROR;
-	}
-	if (get_sign(&src) == 0 && src.bit[0] > 2147483647) {
-		return CONVERTING_ERROR;
-	}
-	if (get_sign(&src) == 1 && src.bit[0] > 2147483647u) {
-		return CONVERTING_ERROR;
-	}
-	if (src.bit[1] != 0|| src.bit[2] != 0) {
-		return CONVERTING_ERROR; 
-	}
-	*dst = (int)src.bit[0];
+int s21_from_decimal_to_int(s21_decimal src, int *dst) {
 
-  if (src.bit[3] & (1 << 31)) {
-    *dst = -(*dst);
-  }
+    int error_flag = 0;
 
-  return OK;
+    if (!dst) {
+        error_flag = 1;
+    } else {
+        s21_decimal truncated;
+        if (!error_flag) {
+            int truncate_result = s21_truncate(src, &truncated);
+            if (truncate_result != 0) {
+                error_flag = 1;
+            }
+        }
+
+        if (!error_flag) {
+            if (get_sign(&truncated) == 0 && truncated.bit[0] > 2147483647) {
+                error_flag = 1;
+            } else if (get_sign(&truncated) == 1 && truncated.bit[0] > 2147483647u) {
+                error_flag = 1;
+            } else if (truncated.bit[1] != 0 || truncated.bit[2] != 0) {
+                error_flag = 1;
+            }
+        }
+
+        if (!error_flag) {
+            *dst = (int)truncated.bit[0];
+            if (truncated.bit[3] & (1 << 31)) {
+                *dst = -(*dst);
+            }
+        }
+    }
+
+    return error_flag;
 }
